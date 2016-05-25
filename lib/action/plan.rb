e32849cb6a605bbab0019bd4d32333fd09bb4178
Artem Baguinski <abaguinski@depraktijkindex.nl>
2016-05-25 15:56:24 +0200

    more better sequence status

--- lib/action/plan.rb
+++ lib/action/plan.rb
@@ -29,33 +29,7 @@ module Action
     end
 
     def status
-      chunks = schedule.map(&:status).chunk{|status|status}
-      statuses = chunks.map(&:first)
-      chunks = chunks.map(&:last)
-      current = nil
-      case chunks.count
-      when 0
-        return :empty
-      when 1
-        status = statuses.first
-        return status if [:done, :planned].include?(status)
-        current = 0
-      when 2
-        current = if statuses.first == :done
-                    1
-                  elsif statuses.last == :planned
-                    0
-                  end
-      when 3
-        current = 1 if statuses.first == :done && statuses.last == :planned
-      end
-      if current
-        status = statuses[current]
-        if [:failed, :running].include?(status) && chunks[current].length == 1
-          return status
-        end
-      end
-      :invalid
+      Action::State.sequence_status(schedule.map(&:status))
     end
 
     # let action plan itself
