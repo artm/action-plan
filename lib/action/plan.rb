f0562cb17912197125cbe23d5a13b66f27d4dfa0
Artem Baguinski <abaguinski@depraktijkindex.nl>
2016-05-26 08:02:12 +0200

    plan code reordering

--- lib/action/plan.rb
+++ lib/action/plan.rb
@@ -17,14 +17,6 @@ module Action
       end
     end
 
-    def run_action action, state
-      state.status = :running
-      action.run
-      state.status = :done
-    rescue
-      state.status = :failed
-    end
-
     def action_states
       schedule.dup
     end
@@ -36,18 +28,6 @@ module Action
     RunnableStatuses = [:planned, :failed, :empty]
     def runnable?
       RunnableStatuses.include?(status)
-    end
-
-    # let action plan itself
-    def plan_action action_class, &block
-      action_class.new.configure(&block).expand_into(plan: self)
-    end
-
-    # schedule action execution at the current point in the plan
-    def schedule_action action_class, config
-      new_state = Action::State.new(action_class: action_class, config: config)
-      schedule << new_state
-      new_state.status = :planned
     end
 
     class DSL
@@ -64,8 +44,28 @@ module Action
     class Error < RuntimeError ; end
     class NotRunnable < Error ; end
 
+    # let action plan itself
+    def plan_action action_class, &block
+      action_class.new.configure(&block).expand_into(plan: self)
+    end
+
+    # schedule action execution at the current point in the plan
+    def schedule_action action_class, config
+      new_state = Action::State.new(action_class: action_class, config: config)
+      schedule << new_state
+      new_state.status = :planned
+    end
+
     private
 
     attr_reader :schedule
+
+    def run_action action, state
+      state.status = :running
+      action.run
+      state.status = :done
+    rescue
+      state.status = :failed
+    end
   end
 end
